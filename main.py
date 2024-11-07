440db86b13f7ee49872d59bf1d287590aa31577f
snusd0sa <snusd0sa@proton.me>
2024-11-07 21:40:00 +0100

    add circleshape.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main ():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -19,7 +22,7 @@ def main ():
 
         # Update the screen with what we've drawn
         pygame.display.flip() 
-
+        dit = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
